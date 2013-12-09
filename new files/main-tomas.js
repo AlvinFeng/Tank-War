bac8cf937695c4eede65695b19f3fbb8fa34410c
Michael Sanchez <michael.sanchez@mindbodyonline.com>
2013-12-08 20:48:36 -0800

    Re added working title sreen

--- new files/main-tomas.js
+++ new files/main-tomas.js
@@ -44,9 +44,9 @@ window.onload = function() {
                     'sounds/laserShot.wav'
 					);
 
-    game.onload = function() {
-        game.pushScene(new GameScene());
-        //game.pushScene(new TitleScene());        
+    game.onload = function() { 
+        game.pushScene(new GameScene());    
+        game.pushScene(new TitleScene());       
     };
 
     GameScene = Class.create(Scene, 
@@ -66,7 +66,6 @@ window.onload = function() {
 
         }
     });
-
     TitleScene = Class.create(Scene, 
     {
         initialize: function(){
@@ -74,41 +73,59 @@ window.onload = function() {
             BulletUpgrade.TitleScreen = true;
             var titleBG = new Sprite(1422, 800);
             titleBG.image = game.assets["images/blackBG.png"];
+            titleBG.ontouchend = function () {
+                if (TitleScene.StartGame == true) {
+                    game.popScene();
+                    game.pushScene(new Level1Scene());
+                    BulletUpgrade.TitleScreen = false;
+                }
+            };
             this.addChild(titleBG);
             //game.assets['sounds/bgMusic2.mp3'].play();
-            //world = new World(0,'cyan','red');
-            var tank = new PlayerTank(100,100,'blue','blue',world);
-            tank.rotateWholeTank(90);
-            tank.scale(5, 5);
-            tank.x = 325;
-            tank.y = 200;
-            this.addChild(tank);
+            world = new World(0,'cyan','red', this);
+            this.tank = new PlayerTank(100,100,'blue','blue',world);
+            this.tank.rotateWholeTank(90);
+            this.tank.scale(5, 5);
+            this.tank.x = 325;
+            this.tank.y = 200;
+            this.addChild(this.tank);
 
-            var bUpgrade = new BulletUpgrade(150, 50, this, 1);
-            bUpgrade.scale(3, 3);
-            this.addChild(bUpgrade);
+            this.bUpgrade = new BulletUpgrade(150, 50, this, 1);
+            this.bUpgrade.scale(3, 3);
+            this.addChild(this.bUpgrade);
 
-            bUpgrade = new BulletUpgrade(350, 50, this, 2);
-            bUpgrade.scale(3, 3);
-            this.addChild(bUpgrade);
+            this.bUpgrade2 = new BulletUpgrade(350, 50, this, 2);
+            this.bUpgrade2.scale(3, 3);
+            this.addChild(this.bUpgrade2);
 
-            bUpgrade = new BulletUpgrade(550, 50, this, 3);
-            bUpgrade.scale(3, 3);
-            this.addChild(bUpgrade);
+            this.bUpgrade3 = new BulletUpgrade(550, 50, this, 3);
+            this.bUpgrade3.scale(3, 3);
+            this.addChild(this.bUpgrade3);
 
             var titleBG = new Sprite(236, 48);
             titleBG.image = game.assets["images/start.png"];
             titleBG.ontouchend = function () {
                 game.assets["sounds/select.wav"].play();
-                game.popScene();
-                BulletUpgrade.TitleScreen = false;
+                game.currentScene.removeChild(this);
+                game.currentScene.removeChild(game.currentScene.tank);
+                game.currentScene.removeChild(game.currentScene.bUpgrade);
+                game.currentScene.removeChild(game.currentScene.bUpgrade2);
+                game.currentScene.removeChild(game.currentScene.bUpgrade3);
+
+                var storyText = new enchant.Label("It is the year 3500 and the world is currently at war! Tank warfare and technology has progressed immensely and all warfare is now done online. You are the head commander of an elite tank platoon collectively known as The Earth Defenders. A group of cyber terrorists recently appeared in the Battle Net (where all warfare happens) and have been spotted trying to take it over! If the Battle Net is lost to the cyber terrorists then the world will collectively lose access to the internet! As the lead commander you are responsible for leading the charge against enemy tank platoons as well as going on covert missions deep into enemy cyber space.");
+                storyText.color = "00FF00";//"white";
+                storyText.textAlign = "center";
+                storyText.scale(2, 2);
+                storyText.x = 200;
+                storyText.y = 100;
+                game.currentScene.addChild(storyText);
+                TitleScene.StartGame = true;               
             };
             titleBG.x = 250;
             titleBG.y = 350;
             this.addChild(titleBG);
         }
     });
-
 	
     //start game
     game.start();
