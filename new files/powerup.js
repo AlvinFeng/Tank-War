790cfd05012615b1903c016a4db616f03aa5ce0b
thsteven13 <t.h.steven.13@gmail.com>
2013-12-08 21:56:21 -0800

    deleted some bugs

--- new files/powerup.js
+++ new files/powerup.js
@@ -17,19 +17,7 @@ Powerup = enchant.Class.create(enchant.Sprite,
 		this.currentScene = scene;
 	},
 	onenterframe : function () {
-            if (this.within(this.currentWorld.playerTank, 30)) {
-                this.currentScene.removeChild(this);
-                game.assets['sounds/upgrade.wav'].play();
-				if(this.type==0)
-				{
-				
-				}
-				if(this.type==1)
-				{
-					this.currentScene.playerTank.movement++;
-					this.currentScene.playerTank.movement++;
-				}
-            }
+
         if (this.rotation >= 355)
                 this.rotation = 0;
             this.rotation++;
